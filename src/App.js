59eab276a2bfaeb034920ca477e679a64b0634a1
tranbrandon1233 <70561420+tranbrandon1233@users.noreply.github.com>
2023-01-04 18:11:11 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -670,10 +670,10 @@ class App extends Component {
           <div><b> Congratulations! </b></div>
         </div>
       <div style={{display: this.state.showCorrect ? 'block': 'none' }} id="correctSelected" class="correctAns">
-          <t>That's correct!</t><br/>
+          <t>That's correct!</t><br/><br/>
         </div>
         <div style={{display: this.state.showWrong ? 'block': 'none' }} id="wrongSelected" class="wrongAns">
-          <t>Sorry, that's not correct.</t><br/>
+          <t>Sorry, that's not correct.</t><br/><br/>
         </div>
           <div style={{display: this.state.showWords ? 'block': 'none' }}>
             <t>{words[this.state.index]}</t><br/>
